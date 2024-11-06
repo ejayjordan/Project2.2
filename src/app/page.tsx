d8a901c82d160792ee9135f63fcf87b7a8e04eac
jay <emma.jordan@bsu.edu>
2024-11-06 11:30:11 -0500

    deletion of unnecessary folders/pages; Creation of boolean pages with router refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,42 @@ import React from 'react'
 import { useRouter } from 'next/navigation'
 
 
+
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
+
+  function homePage(){
+    let homePage = true;
+    let viewCards = false;
+    let addCards=false;
+    router.refresh()
+  }
+
+  function viewCards(){
+    let homePage = false;
+    let viewCards = true;
+    let addCards = false;
+    router.refresh()
+  }
+
+  function addCards(){
+    let homePage = false;
+    let viewCards = false;
+    let addCards = true;
+    router.refresh()
+  }
 
   return (
     <>
-    <li><button onClick={() => router.push('/flashcards')}>
+    <li><button onClick={homePage}>
     Play Flashcards
     </button></li>
 
-    <li><button onClick={() => router.push('/difficulty')}>
+    <li><button onClick={viewCards}>
     Change Difficulty
     </button></li>
 
-    <li><button onClick={() => router.push('/study')}>
-    Study Flashcards
-    </button></li>
-
-    <li><button onClick={() => router.push('/add')}>
+    <li><button onClick={addCards}>
     Add Flashcards
     </button></li>
     
