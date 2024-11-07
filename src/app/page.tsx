fbb0b7aeaf8060bdd80312c620590531a0ca6639
jay <emma.jordan@bsu.edu>
2024-11-06 19:24:28 -0500

    testing homePage 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,13 @@ import { useRouter } from 'next/navigation'
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
-const router = useRouter();
+
+let homePage = true
+let viewCards = false
+let addCards = false
 
 export default async function Home() {
+  const router = useRouter();
   const cards = await prisma.flashcards.findMany();
   console.log(cards)
   const cardDisplay = cards.map((cards, index) => <li key  = {index}>{cards.prompt}</li>)
@@ -15,7 +19,7 @@ export default async function Home() {
 function homePage(){
   let homePage = true;
   let viewCards = false;
-  let addCards=false;
+  let addCards = false;
   router.refresh()
  }
 
